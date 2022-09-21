b49557d62eebd6a92dd48d2d8766a58e1d8f322f
Katarina Van De Pontseele <katavdp@gmail.com>
2022-09-21 09:52:28 +0200

    Lagde filstrukturen til JCDecaux-dashboardet og importerte den i App.tsx

--- src/dashboards/JCDecaux/index.tsx
+++ src/dashboards/JCDecaux/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
-const JCDecaux = (): JSX.Element | null => <div>Hei!</div>
+import Content from './Content'
+import Footer from './Footer'
+import Header from './Header'
+
+import './styles.scss'
+
+const JCDecaux = (): JSX.Element | null => (
+    <div className="wrapper">
+        <Header />
+        <Content />
+        <Footer />
+    </div>
+)
 
 export default JCDecaux
